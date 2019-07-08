18acb75973e6913dea577f0ee1983ada6231e2c5
AidiRi <rivera.aidi@gmail.com>
2019-07-08 15:15:34 -0700

    created database/resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+chris = User.create(name: "Chris")
+aidi = User.create(name: "Aidi")
+
+p1 = Project.create(name: "home", user_id: chris.id)
+p2 = Project.create(name: "work", user_id: aidi.id)
+
+t1 = Task.create(name: "shop", status:"closed", project_id: p1.id);
+t2 = Task.create(name: "eat", status:"open", project_id: p1.id);
+t3 = Task.create(name: "write stuff", status:"closed", project_id: p2.id);
+t4 = Task.create(name: "do stuff", status:"open", project_id: p2.id);
