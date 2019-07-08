afa0702f4f1c82f53e31c7be73ab344d0c8ad70a
AidiRi <rivera.aidi@gmail.com>
2019-07-08 15:58:08 -0700

    project/task controller action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,22 @@
 class ProjectsController < ApplicationController
+
+  def index
+    projects = Project.all
+    render json: projects,
+      include: [:user =>
+        {:except =>
+          [:created_at, :updated_at]}],
+      except: [:created_at, :updated_at]
+  end
+
+  def create
+    project = Project.create(project_params)
+  end
+
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :user_id)
+  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,21 @@ class TasksController < ApplicationController
 
   def index
     tasks = Task.all
-    render json: tasks, include: [:project]
+    render json: tasks,
+      include: [:project =>
+        {:except =>
+          [:created_at, :updated_at]}],
+      except: [:created_at, :updated_at]
   end
 
+  def create
+    task = Task.create(task_params)
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :project_id, :status)
+  end
 end
